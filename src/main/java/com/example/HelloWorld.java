72286c4d9635935440486c755835f0b5dc6f353d
cyclopserulz <109838776+cyclopserulz@users.noreply.github.com>
2024-12-09 16:24:37 +0530

    Create HelloWorld.java

---
+++ src/main/java/com/example/HelloWorld.java
@@ -0,0 +1,8 @@
+# Create a sample Java file
+echo 'package com.example;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}'
